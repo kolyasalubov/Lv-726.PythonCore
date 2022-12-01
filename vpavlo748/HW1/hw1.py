8f422c6098bf957acc04aabbc57568faf49be43f
Pavlo Vysotskyi <vpavlo2021@gmail.com>
2022-12-01 21:29:22 +0200

    HW

---
+++ vpavlo748/HW1/hw1.py
@@ -0,0 +1,16 @@
+a = int(input("First number: "))
+b = int(input("Second number: "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+
+name = input('What is your name?')
+age = input('How old are you?')
+live = input('Where do you live?')
+
+print("Hello,",name)
+print("Your age is",age)
+print("You live in",live)
