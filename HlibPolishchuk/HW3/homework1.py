dfaf0192ccfea53a59bd87021b70c5cee68c62e3
Hlib Polishchuk <gleb.polishchuk.03@gmail.com>
2022-12-01 13:03:44 +0200

    added homework3 HlibPolishchuk

--- HlibPolishchuk/HW3/homework1.py
+++ HlibPolishchuk/HW3/homework1.py
@@ -1,50 +1,50 @@
-# #1
-#
-# q = ("Beautiful is better than ugly.")
-# w = ("Explicit is better than implicit.")
-# e = ("Simple is better than complex.")
-# r = ("Complex is better than complicated.")
-# t = ("Flat is better than nested.")
-# z = ("Sparse is better than dense.")
-# y = ("Readability counts.")
-# u = ("Special cases arent special enough to break the rules.")
-# i = ("Although practicality beats purity.")
-# o = ("Errors should never pass silently.")
-# p = ("Unless explicitly silenced.")
-# a = ("In the face of ambiguity, refuse the temptation to guess.")
-# s = ("There should be one-- and preferably only one --obvious way to do it.")
-# d = ("Although that way may not be obvious at first unless youre Dutch.")
-# f = ("Now is better than never.")
-# g = ("Although never is often better than *right* now.")
-# h = ("If the implementation is hard to explain its a bad idea.")
-# j = ("If the implementation is easy to explain it may be a good idea.")
-# k = ("Namespaces are one honking great idea -- lets do more of those!")
-#
-# zen = q+w+e+r+t+z+y+u+i+o+p+a+s+d+f+g+h+j+k
-#
-# print(str(zen))
-#
-#
-# print(zen.upper())
-#
-#
-# better_occurrences = zen.count("better")
-# never_occurrences = zen.count("never")
-# is_occurrences = zen.count("is")
-#
-# print('Number of occurrences of the word "better" :', better_occurrences)
-# print('Number of occurrences of the word "never" :', never_occurrences)
-# print('Number of occurrences of the word "is" :', is_occurrences)
-#
-#
-#
-# print(zen.replace('i', '&'))
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#1
+
+q = ("Beautiful is better than ugly.")
+w = ("Explicit is better than implicit.")
+e = ("Simple is better than complex.")
+r = ("Complex is better than complicated.")
+t = ("Flat is better than nested.")
+z = ("Sparse is better than dense.")
+y = ("Readability counts.")
+u = ("Special cases arent special enough to break the rules.")
+i = ("Although practicality beats purity.")
+o = ("Errors should never pass silently.")
+p = ("Unless explicitly silenced.")
+a = ("In the face of ambiguity, refuse the temptation to guess.")
+s = ("There should be one-- and preferably only one --obvious way to do it.")
+d = ("Although that way may not be obvious at first unless youre Dutch.")
+f = ("Now is better than never.")
+g = ("Although never is often better than *right* now.")
+h = ("If the implementation is hard to explain its a bad idea.")
+j = ("If the implementation is easy to explain it may be a good idea.")
+k = ("Namespaces are one honking great idea -- lets do more of those!")
+
+zen = q+w+e+r+t+z+y+u+i+o+p+a+s+d+f+g+h+j+k
+
+print(str(zen))
+
+
+print(zen.upper())
+
+
+better_occurrences = zen.count("better")
+never_occurrences = zen.count("never")
+is_occurrences = zen.count("is")
+
+print('Number of occurrences of the word "better" :', better_occurrences)
+print('Number of occurrences of the word "never" :', never_occurrences)
+print('Number of occurrences of the word "is" :', is_occurrences)
+
+
+
+print(zen.replace('i', '&'))
+
+
+
+
+
+
+
+
+
