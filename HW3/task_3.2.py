42a912a9829aa16734e3c7ac23f86f8beb64d596
Serhii Hladyr <shladyr@cisco.com>
2022-12-02 15:28:39 +0200

    Add 3.2 task with Update

---
+++ HW3/task_3.2.py
@@ -0,0 +1,28 @@
+# 4-digit numbers calculating
+f4_number = input("Enter four-digit number:")
+
+# Multiplication of every sign sequencently
+multiply = 1
+print("Way-1")
+for y in f4_number:
+    multiply *= int(y)
+print(f"Multiplication of every sign sequencently is {multiply}")
+print("Way-2")
+multiply = int(f4_number[0]) * int(f4_number[1]) * int(f4_number[2]) * int(f4_number[3])
+print(f"Multiplication of every sign sequencently is {multiply}")
+
+# Reverse order of digits
+print("Way-1")
+reversed_f4_number_in_string = "".join(reversed(f4_number))
+print(f"Reversed order of digits - {reversed_f4_number_in_string}")
+print("Way-2")
+reversed_f4_number_in_string = f4_number[3] + f4_number[2] + f4_number[1] + f4_number[0]
+print(f"Reversed order of digits - {reversed_f4_number_in_string}")
+
+# Sort digits' sequence
+print("Way-1")
+sorted_sequence = "".join(sorted(f4_number))
+print(f"Sorted digits' sequence - {sorted_sequence}")
+print("Way-2")
+sorted_sequence = sorted(f4_number)
+print(f"Sorted digits' sequence - {sorted_sequence}")
