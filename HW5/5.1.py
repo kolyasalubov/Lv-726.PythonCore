087e13822701070e633d6b695ea4200629f64394
Serhii Hladyr <shladyr@cisco.com>
2022-12-05 18:39:57 +0200

    print("==========  Calculations for RANGE=1:11

--- HW5/5.1.py
+++ HW5/5.1.py
@@ -1,14 +1,14 @@
 # Author: Serhii
 # HW5 task 5.1
 # input the MAX number to which you want to send
-max_num = 11
-print("==========  Calculations for RANGE=1:10  ==========")
+MAX = 11
+print("==========  Calculations for RANGE=1:11  ==========")
 
 # check if number is divisible by 2 and 3
 n = 1  # starting numbers from 0
 # run until it reaches Max number
 print("Numbers not divisible by 2 and 3")
-while n <= max_num:
+while n <= MAX:
     if n % 2 != 0 and n % 3 != 0:
         print(n, ", Memory ID =",id(n))
     n = n + 1       # incrementing the counter
@@ -17,7 +17,7 @@ while n <= max_num:
 n = 1  # starting numbers from 0
 # run until it reaches Max number
 print("Numbers are divisible by 3 - odd!")
-while n <= max_num:
+while n <= MAX:
     if n % 3 == 0:
         print(n, ", Memory ID =",id(n))
     n = n + 1       # incrementing the counter
@@ -27,7 +27,7 @@ while n <= max_num:
 n = 1  # starting numbers from 0
 # run until it reaches Max number
 print("Numbers are divisible by 2 - even!")
-while n <= max_num:
+while n <= MAX:
     if n % 2 == 0:
         print(n, ", Memory ID =",id(n))
     n = n + 1       # incrementing the counter
