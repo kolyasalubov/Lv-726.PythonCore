150cb156e8dad55bd5d6495128f7283bac22af4b
Serhii Hladyr <shladyr@cisco.com>
2022-12-05 18:33:30 +0200

    Add 5.1

---
+++ HW5/5.1.py
@@ -0,0 +1,34 @@
+# Author: Serhii
+# HW5 task 5.1
+
+
+# input the MAX number to which you want to send
+max_num = 11
+
+# check if number is divisible by 2 and 3
+n = 1  # starting numbers from 0
+# run until it reaches Max number
+print("Numbers not divisible by 2 and 3")
+while n <= max_num:
+    if n % 2 != 0 and n % 3 != 0:
+        print(n)
+    n = n + 1       # incrementing the counter
+
+# check if number is divisible by 3
+n = 1  # starting numbers from 0
+# run until it reaches Max number
+print("Numbers are divisible by 3 - odd!")
+while n <= max_num:
+    if n % 3 == 0:
+        print(n)
+    n = n + 1       # incrementing the counter
+
+
+# check if number is divisible by 2
+n = 1  # starting numbers from 0
+# run until it reaches Max number
+print("Numbers are divisible by 2 - even!")
+while n <= max_num:
+    if n % 2 == 0:
+        print(n)
+    n = n + 1       # incrementing the counter
