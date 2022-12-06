9370885f7d9d746e5c063b83dba01ba401a4df53
Liubov Koliasa <kolyasa.lubov@gmail.com>
2022-12-06 10:45:12 +0200

    update all folder

---
+++ 1.py
@@ -0,0 +1,43 @@
+# a = {90 + 89 + 
+# 45 + 67 + 78}
+
+# print(a)
+
+# R1 = "hello\name\table"
+# R2 = r"hello\name\table"
+
+# print(R1)
+# print(R2)
+
+# name = "John"
+# age = 23
+# salary = 999.99
+# print("%s is %d years old. Your sallary is %.3f $" % (name, age, salary))
+
+# default_order = "{}, {} and {}".format('John','Bill','Sean')
+# print(default_order)
+# # order using positional argument
+# positional_order = "{1}, {0} and {2}".format('John','Bill','Sean')
+# print(positional_order)
+# order using keyword argument
+
+# j='John'
+# b='Bill'
+# s='Sean'
+# keyword_order = f"{s}, {b} and {j}"
+# print(keyword_order)
+
+str = 'programiz'
+print('str = ', str)
+
+#first character
+print('str[0] = ', str[0])
+
+#last character
+print('str[-1] = ', str[-1])
+
+#slicing 2nd to 5th character
+print('str[1:5] = ', str[1:5])
+
+#slicing 6th to 2nd last character
+print('str[5:-2] = ', str[5:-4])
--- maryanah/HW7/secret_message.py
+++
@@ -1,4 +0,0 @@
-def greet(name):
-    if name == "Johnny":
-        return "Hello, my love!"
-    return f"Hello, {name}!"
