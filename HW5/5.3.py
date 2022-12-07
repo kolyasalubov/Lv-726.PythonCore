e11fbb6e2e5480517fb0df4a76d9c424bf7d3f84
Serhii Hladyr <shladyr@cisco.com>
2022-12-07 14:56:58 +0200

    Add 5.3

--- HW5/5.3.py
+++ HW5/5.3.py
@@ -2,3 +2,15 @@
 # Print Fibonacci numbers up to the entered number n,
 # using cycles. (Sequence of Fibonacci numbers 0, 1, 1, 2, 3, 5, 8, 13, etc.)
 
+# VARIABLES
+first = 0
+second = 1
+def printFibonacci():
+    global first, second
+    count = int(input('Enter the TOTAL count of numbers: '))
+    while count >= 0:
+        first, second = second, (first + second)
+        count -= 1
+        print(first)
+
+printFibonacci()
