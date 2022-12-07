dd9f5166112f4b0f45101d593bc4432b157505b4
Liubov Koliasa <kolyasa.lubov@gmail.com>
2022-12-07 23:49:50 +0200

    update branch

---
+++ Oleh_Pashchyn/HW4/HW4.py
@@ -0,0 +1,10 @@
+number = int(input("Enter number: "))
+
+if number == 0 and 1:
+    factorial = 1
+else:
+    factorial = 1
+    for fact in range(1, number + 1):
+        factorial = factorial * fact
+
+print(factorial)
---
+++ Oleh_Pashchyn/HW5/HW5_1.py
@@ -0,0 +1,7 @@
+list = [5, 10, 13, 19, 30, 35, 52, 76, 112]
+#print(type(list))
+for flo in list:
+    flo = float(flo)
+    print(f"new float list {flo}")
+
+#print(type(flo))
