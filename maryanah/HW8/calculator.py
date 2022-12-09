d034ecc1d5e074762a09e6329f56fb03b97d400a
Maryana <maryacha23@gmail.com>
2022-12-09 15:54:48 +0200

    Added HW8, calculator

---
+++ maryanah/HW8/calculator.py
@@ -0,0 +1,22 @@
+def add(num1, num2):
+    """This function calculates sum of entered numbers."""
+    sum = num1 + num2
+    return sum
+
+def subtraction(num1, num2):
+    """This function calculates substraction
+    of two entered numbers."""
+    sub = num1 - num2
+    return sub
+
+def multiplication(num1, num2):
+    """This function calculates multiplication
+    of two entered numbers."""
+    mult = num1 * num2
+    return mult
+
+def division(num1, num2):
+    """This function calculates division
+    of two entered numbers."""
+    div = num1 / num2
+    return div
