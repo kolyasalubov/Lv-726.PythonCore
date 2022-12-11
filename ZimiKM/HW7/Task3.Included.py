ae5f1dc469fc9d60cdbd75a15db529351e69faae
ZimiKM <zimikm@gmail.com>
2022-12-11 20:10:20 +0200

    added coments and upgrade input and output

--- ZimiKM/HW7/Task3.Included.py
+++ ZimiKM/HW7/Task3.Included.py
@@ -1,10 +1,21 @@
-def count_mach(input_string: st) -> dict:
-    mach_dict = {key: 0 for key in input_string}
+def count_match(string: str) -> dict:
+    '''Find number of included char in string
+    Parameter:string - target string
+    
+    Result: return dictionary (match_dict) with 
+    char as key and  number of included as value'''
 
-    for key in mach_dict:
-        count = input_string.count(key)
-        mach_dict.update({key: count})
-    return mach_dict
+    #kreated dict with list of char from string
+    match_dict = {key: 0 for key in string}
+
+    #counting match of char in string
+    for key in match_dict:
+        count = string.count(key)
+        match_dict.update({key: count})
+    return match_dict
 
 
-print(count_mach('hello'))
+#Input string and output result
+user_string = input('Enter the string: ')
+result = count_match(user_string)
+print(result)
