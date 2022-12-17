5e4ea589f1651ae5240cf9b1a93a4a3fbf95014c
Hlib Polishchuk <gleb.polishchuk.03@gmail.com>
2022-12-17 15:44:05 +0200

    added HW4

--- ZimiKM/HW7/Task1. LargestNumber.py
+++
@@ -1,6 +0,0 @@
-def largestNumber(number1, number2: int) -> str:
-    #find the largest number
-    return max(number1, number2)
-
-
-print(largestNumber(123, -234))
--- maxim_g/HW_CODE_WARS_2/is_this_my_tail.py
+++
@@ -1,9 +0,0 @@
-def correct_tail(body, tail):
-    sub = body[len(body)-1]
-    if sub == tail:
-        return True
-    return False
-
-
-result = correct_tail("Fox", "x")
-print(result)
